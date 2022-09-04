be932af5a8e162cb7ab6d4d2eddfa98a9897a267
Yogeshharsana <e20cse331@bennett.edu.in>
2022-09-05 01:42:20 +0530

    latest version changesssssssssssss

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,10 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import { Metaplex } from '@metaplex-foundation/js';
-import { clusterApiUrl, Connection, PublicKey } from '@solana/web3.js';
-import { useEffect, useState } from 'react';
+import { Metaplex } from "@metaplex-foundation/js";
+import { Connection, clusterApiUrl } from "@solana/web3.js";
 
-const connection = new Connection(clusterApiUrl('mainnet-beta'));
-const mx = Metaplex.make(connection);
+const connection = new Connection(clusterApiUrl("mainnet-beta"));
+const metaplex = new Metaplex(connection);
 
 export default function Home() {
   const [address, setAddress] = useState(
